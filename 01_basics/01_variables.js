e837906d0e645b593b15ae0fb038f6e6d07bfb82
MM-WA <waleedasif900@gmail.com>
2023-09-15 10:50:34 +0500

    Learned about variables is js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1122334455
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123qwe"
+accountCity = "lahore"
+let accountState
+
+// accountid = 5 //not allowed
+
+/*
+    Don't use var
+    Because it create issues in block and functional scope
+*/
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
