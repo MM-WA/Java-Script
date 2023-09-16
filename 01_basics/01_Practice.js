24fae2bcd19b0475a534c50754f0375d65e4acc1
MM-WA <waleedasif900@gmail.com>
2023-09-16 12:10:35 +0500

    Did practice of const, let, var

---
+++ 01_basics/01_Practice.js
@@ -0,0 +1,19 @@
+const accountId = 1122334455
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123qwe"
+accountCity = "Lahore"
+var accountStatus
+
+//const
+//let accountId //no redeclaration
+//accountId = 5 //no reassigining
+
+//let
+//var accountEmail //no redecleration
+//accountEmail = "def@gmail.com" //only reassigning
+
+var accountPassword //can be redecleare
+ accountPassword = "321ewq" /*can be reassigned (this will also change previous accountPassword
+                            accutally it will override previous accountPassword)*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStatus])
