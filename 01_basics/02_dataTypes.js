2895a6a56d02ff58d67bf3c517e6b9493437e41e
MM-WA <waleedasif900@gmail.com>
2023-09-18 14:58:16 +0500

    ...

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,20 @@
 "use strict" //treat all JS code as newer version
+
+//alert(3 + 3)  //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => niqueness
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
