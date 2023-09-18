a394b619fed3fe7a6c8ba18d2933fed485a2e5d7
MM-WA <waleedasif900@gmail.com>
2023-09-18 16:35:49 +0500

    Arithmetic Operations study

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -29,5 +29,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let number = 45
 
 let stringNumber = String(number)
-console.log(typeof(stringNumber))
-console.log(stringNumber)
+// console.log(typeof(stringNumber))
+// console.log(stringNumber)
+
+
+// ************************* Operations ***************************
+
+let value = 3
+let negValue = - value
+
+// console.log(negValue)
+
+// console.log(2 + 3)
+// console.log(2 - 3)
+// console.log(2 * 3)
+// console.log(2 ** 4)
+// console.log(2 / 4)
+// console.log(2 % 4)
+
+let str1 = "hello" 
+let str2 = " waleed"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter)
---
+++ 01_basics/Practice_03.js
@@ -0,0 +1,30 @@
+// let score = "33"
+// let score = "33abc"
+// let score = "abc"
+// let score = null
+// let score = undefined
+let score = true
+// console.log(typeof score)
+
+let numberScore = Number(score)
+// console.log(typeof numberScore)
+// console.log(numberScore)
+
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "waleed"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn)
+
+
+let nubmer = 48
+
+let stringNumber = String(nubmer)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
