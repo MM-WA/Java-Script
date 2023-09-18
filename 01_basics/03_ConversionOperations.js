f96d655b60d6b811d46ed6970fff559537d98d20
MM-WA <waleedasif900@gmail.com>
2023-09-18 15:48:55 +0500

    Conversion of data types in JS

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,33 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+
+// "33" => 33
+// "33abc" => NaN
+// "abc" => NaN
+// true => 1, false => 0
+// undefined => NaN
+// null => 0
+
+let isLoggedIn = "waleed"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false
+// "waleed" => true
+
+
+let number = 45
+
+let stringNumber = String(number)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
---
+++ 01_basics/Practice_02.js
@@ -0,0 +1,22 @@
+"use strict" //treates JS code as newer version
+
+//alert(3 + 3) //we are using nodejs not browser
+
+console.log(3
+    +
+    3)  //code readibility should be high
+console.log("waleed")
+
+
+// number
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined
+// symbol => uniqueness
+
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
