89fbc11822fb3dc4190e8948bff88c93de17ee22
MM-WA <waleedasif900@gmail.com>
2023-09-27 11:59:03 +0500

    Learned Control Structures in JS

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,49 @@
+// if
+
+let isLoggedIn = true
+let temperature = 41
+
+// if(temperature < 50) {
+//     console.log("Less then 50")
+// } else {
+//     console.log("greater then 50")
+// }
+
+// console.log("executed")
+
+// <, >, <=, >=, !=, ===, !==
+
+
+let score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+
+// console.log(`User power: ${power}`)
+
+// if(score > 100) console.log("test"), console.log("test2")
+
+// if(score < 100) {
+//     console.log("Less then 100")
+// } else if(score < 150) {
+//     console.log("Less then 150")
+// } else if(score < 200) {
+//     console.log("Less then 200")
+// } else {
+//     console.log("Less then 250")
+// }
+
+ const userLoggedIn = true
+ const debitCard = true
+ const loggedInFromGoogle = false
+ const loggedInFromEmail = true
+
+ if(userLoggedIn && debitCard && 2 == 3) {
+    console.log("Allowed to buy")
+ }
+
+ if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in")
+ }
